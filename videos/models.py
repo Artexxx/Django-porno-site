2df9d9a07cc73900f4c5e3da152466ca818c0143
oleg <lizabelakova@gmail.com>
2021-04-02 21:56:15 +0300

    Youtube

--- videos/models.py
+++ videos/models.py
@@ -4,7 +4,7 @@ from django.db.models import Model
 
 class Video(Model):
     title = models.CharField(verbose_name="Название", max_length=100, default="")
-    desc = models.TextField(verbose_name='Описание', max_length=10000, default='', blank=True)
+    desc = models.TextField(verbose_name='Описание', max_length=1000, default='', blank=True)
 
     image = models.FileField(verbose_name="Картика", upload_to="images")
     video = models.FileField(verbose_name="Видео", upload_to="videos")
@@ -13,6 +13,7 @@ class Video(Model):
 
     views = models.IntegerField(verbose_name="Количество просмотров", blank=True, default=0)
     likes = models.IntegerField(verbose_name="Количество лайков", blank=True, default=0)
+    dislikes = models.IntegerField(verbose_name="Количество дизлайков", blank=True, default=0)
 
 
     def __str__(self):
